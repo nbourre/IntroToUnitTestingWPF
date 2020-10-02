47c7fde0b622957b3845b786643a6fa59b336783
Nicolas Bourré <nbourrereg@hotmail.com>
2020-10-02 15:03:58 -0400

    Reverted some good code to wrong code for exercice purpose.

--- DemoLibrary/DataAccess.cs
+++ DemoLibrary/DataAccess.cs
@@ -10,35 +10,17 @@ namespace DemoLibrary
 
         public static void AddNewPerson(PersonModel person)
         {
+            List<string> lines = new List<string>();
             List<PersonModel> people = GetAllPeople();
 
-            AddPersonToPeopleList(people, person);
-
-            List<string> lines = ConvertModelsToCSV(people);
+            foreach(PersonModel user in people)
+            {
+                lines.Add($"{user.FirstName}, {user.LastName}");
+            }
 
             File.WriteAllLines(personTextFile, lines);
         }
 
-        public static void AddPersonToPeopleList(List<PersonModel> people, PersonModel person)
-        {
-            if (string.IsNullOrWhiteSpace(person.FirstName))
-                throw new ArgumentException("You passed in an invalid parameter", "FirstName");
-
-            if (string.IsNullOrWhiteSpace(person.LastName))
-                throw new ArgumentException("You passed in an invalid parameter", "LastName");
-
-            people.Add(person);
-        }
-
-        public static List<string> ConvertModelsToCSV(List<PersonModel> people)
-        {
-            List<string> output = new List<string>();
-
-            foreach (PersonModel user in people)
-                output.Add($"{user.FirstName},{user.LastName}");
-
-            return output;
-        }
 
         public static List<PersonModel> GetAllPeople()
         {
