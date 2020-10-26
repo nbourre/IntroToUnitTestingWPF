0b9abc486dc1649dcd5709dde007050f2c2f1243
Nick B <nbourrereg@hotmail.com>
2020-10-25 21:39:10 -0400

    ConvertModelsToCSV_HasSameNumberOfLineAsPeople

--- DemoLibrary.Tests/CalculatorTests.cs
+++ DemoLibrary.Tests/CalculatorTests.cs
@@ -59,37 +59,7 @@ namespace DemoLibrary.Tests
 
         }
 
-        [Fact]
-        public void ConvertModelsToCSV_HasSameNumberOfLineAsPeople()
-        {
-            var people = GetSomePeople();
 
-            int expected = 4;
 
-            int actual = DataAccess.ConvertModelsToCSV(people).Count;
-
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void ConvertModelsToCSV_NullListShouldFail()
-        {
-            List<PersonModel> nullList = null;
-
-            Assert.Throws<ArgumentNullException>(() => DataAccess.ConvertModelsToCSV(nullList));
-        }
-
-        private List<PersonModel> GetSomePeople()
-        {
-            var result = new List<PersonModel> { 
-                new PersonModel { FirstName = "Nicolas", LastName = "Bourré" },
-                new PersonModel { FirstName = "Gratien", LastName = "Bourré" },
-                new PersonModel { FirstName = "Alain", LastName = "Bourré" },
-                new PersonModel { FirstName = "Denis", LastName = "Bourré" },
-                
-            };
-
-            return result;
-        }
     }
 }
--- DemoLibrary.Tests/DataAccessTests.cs
+++ DemoLibrary.Tests/DataAccessTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Xunit;
 
@@ -6,6 +7,26 @@ namespace DemoLibrary.Tests
 {
     public class DataAccessTests
     {
+        public class TestDataGenerator : IEnumerable<object[]>
+        {
+            public static IEnumerable<object[]> GetPeopleList()
+            {
+                yield return new object[]
+                {
+                    new List<PersonModel> {
+                        new PersonModel { FirstName = "Nicolas", LastName = "Bourré" },
+                        new PersonModel { FirstName = "Gratien", LastName = "Bourré" },
+                        new PersonModel { FirstName = "Alain", LastName = "Bourré" },
+                        new PersonModel { FirstName = "Denis", LastName = "Bourré" },
+                    }
+                };
+            }
+
+            public IEnumerator<object[]> GetEnumerator() => GetPeopleList().GetEnumerator();
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
         [Fact]
         public void AddPersonToPeopleList_ShouldWork()
         {
@@ -28,6 +49,54 @@ namespace DemoLibrary.Tests
             List<PersonModel> people = new List<PersonModel>();
 
             Assert.Throws<ArgumentException>(param, () => DataAccess.AddPersonToPeopleList(people, newPerson));
+        }
+
+        [Fact]
+        public void ConvertModelsToCSV_HasSameNumberOfLineAsPeople()
+        {
+            var people = GetSomePeople();
+
+            int expected = 4;
+
+            int actual = DataAccess.ConvertModelsToCSV(people).Count;
+
+            Assert.Equal(expected, actual);
+        }
+
+        /// <summary>
+        /// Source : https://hamidmosalla.com/2017/02/25/xunit-theory-working-with-inlinedata-memberdata-classdata/
+        /// </summary>
+        /// <param name="people"></param>
+        [Theory]
+        [MemberData(nameof(TestDataGenerator.GetPeopleList), MemberType = typeof(TestDataGenerator))]
+        public void ConvertModelsToCSV_Theory_HasSameNumberOfLineAsPeople(List<PersonModel> people)
+        {
+            int expected = people.Count;
+
+            int actual = DataAccess.ConvertModelsToCSV(people).Count;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ConvertModelsToCSV_NullListShouldFail()
+        {
+            List<PersonModel> nullList = null;
+
+            Assert.Throws<ArgumentNullException>(() => DataAccess.ConvertModelsToCSV(nullList));
+        }
+
+        private List<PersonModel> GetSomePeople()
+        {
+            var result = new List<PersonModel> {
+                new PersonModel { FirstName = "Nicolas", LastName = "Bourré" },
+                new PersonModel { FirstName = "Gratien", LastName = "Bourré" },
+                new PersonModel { FirstName = "Alain", LastName = "Bourré" },
+                new PersonModel { FirstName = "Denis", LastName = "Bourré" },
+
+            };
+
+            return result;
         }
 
     }
